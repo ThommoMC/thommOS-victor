6edc9dc2de928eff02d934f37bd6cf663bee883f
hamsteronpotato <amy@amymc.dev>
2025-12-22 18:47:12 +1100

    Have unmuting restore to correct previous volume

--- animProcess/src/cozmoAnim/faceDisplay/faceInfoScreenManager.cpp
+++ animProcess/src/cozmoAnim/faceDisplay/faceInfoScreenManager.cpp
@@ -141,8 +141,6 @@ namespace {
 
   // How long the button needs to be pressed for before it should trigger shutdown animation
   CONSOLE_VAR( u32, kButtonPressDurationForShutdown_ms, "FaceInfoScreenManager", 500 );
-
-  CONSOLE_VAR_RANGED(int, kUnmuteVolumeLevel, "FaceInfoScreenManager", 5, 0, 5); // Amy (hamsteronpotato)
 #if ANKI_DEV_CHEATS
   // Fake one of several types of button presses. This value will get reset immediately, so to
   // run it again from the web interface, first set it to NoOp
@@ -1123,15 +1121,13 @@ void FaceInfoScreenManager::ProcessMenuNavigation(const RobotState& state)
     _engineLoaded &&
     CanEnterPairingFromScreen(currScreenName)) {
     if (!_isSpeakerMuted) {
-      RobotInterface::UpdateVolume volume;
-      volume.volumeLevel = 0;
-      RobotInterface::SendAnimToEngine(volume);
+      RobotInterface::ToggleMute muteMsg;
+      RobotInterface::SendAnimToEngine(muteMsg);
       _isSpeakerMuted = true;
       ToggleSpeakerMute("TRIPLE_PRESS");
     } else {
-      RobotInterface::UpdateVolume volume;
-      volume.volumeLevel = kUnmuteVolumeLevel;
-      RobotInterface::SendAnimToEngine(volume);
+      RobotInterface::ToggleMute muteMsg;
+      RobotInterface::SendAnimToEngine(muteMsg);
       _isSpeakerMuted = false;
       ToggleSpeakerMute("TRIPLE_PRESS");
     }
